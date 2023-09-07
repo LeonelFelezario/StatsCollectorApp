18562d9567f56256b237ae43f43c6d6103256428
LeonelFelezario <leonel.felezario@luckycue.tech>
2023-09-07 17:43:55 +0800

    [api] create error handling middleware

--- api/server.js
+++ api/server.js
@@ -14,6 +14,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+
+app.use((req, res, next) =>{
+    console.log(`${req.socket.remoteAddress} is requesting ${req.method}:${req.url}`);
+    next();
+});
+
 //root path
 app.get('/', (req, res) => res.send('Server is ready'));
 
